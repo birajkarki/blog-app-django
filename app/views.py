7a177f03395d43dad77c4bd88ba3d05abf8b92c1
Biraj Karki <birajkarki9849@gmail.com>
2023-09-28 22:53:15 +0545

    completed entire commenting system with replies features

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from app.models import Comments, Post
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from app.forms import CommentForm
 
 # Create your views here.
@@ -13,17 +15,31 @@ def index(request):
 
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
-    comments = Comments.objects.filter(post=post)
+    comments = Comments.objects.filter(post=post, parent=None)
     form = CommentForm()
 
     if request.POST:
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid:
-            comment = comment_form.save(commit=False)
-            postid = request.POST.get('post_id')
-            post = Post.objects.get(id = postid)
-            comment.post = post
-            comment.save()
+            parent_obj = None
+            if request.POST.get('parent'):
+                # save reply
+                parent=request.POST.get('parent')
+                parent_obj = Comments.objects.get(id=parent)
+                if parent_obj:
+                    comment_reply = comment_form.save(commit=False)
+                    comment_reply.parent = parent_obj
+                    comment_reply.post = post
+                    comment_reply.save()
+                    return HttpResponseRedirect(reverse('post_page',kwargs={'slug':slug}))
+
+            else:
+                comment = comment_form.save(commit=False)
+                postid = request.POST.get('post_id')
+                post = Post.objects.get(id = postid)
+                comment.post = post
+                comment.save()
+                return HttpResponseRedirect(reverse('post_page',kwargs={'slug':slug}))
 
     if post.view_count is None:
         post.view_count = 1
